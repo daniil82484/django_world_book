2f773b146c211a0e8a21363531565c086c6b1fca
danii <daniil82484@gmail.com>
2023-01-18 23:29:43 +0500

    first commit

---
+++ catalog/admin.py
@@ -0,0 +1,54 @@
+from django.contrib import admin
+from .models import Author, Book, Genre, Language, Status, BookInstance
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name',
+              ('date_of_birth', 'date_of_death')]
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'genre', 'language', 'display_author')
+    list_filter = ('genre', 'author')
+    inlines = [BookInstanceInline]
+
+
+# @admin.register(BookInstance)
+# class BookInstanceAdmin(admin.ModelAdmin):
+#     list_filter = ('book', 'status')
+#     fieldsets = (
+#         ('Экземпляр книги', {
+#             'fields': ('book', 'imprint', 'inv_nom')
+#          }),
+#         ('Статус и окончание его действия', {
+#             'fields': ('status', 'due_back')
+#          }),
+#     )
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'inv_nom')
+        }),
+        ('Availability', {
+        'fields': ('status', 'due_back', 'borrower')
+        }),
+    )
+
+admin.site.register(Author, AuthorAdmin)
+# admin.site.register(Author)
+
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
+
+admin.site.register(Genre)
+admin.site.register(Language)
+admin.site.register(Status)
